2ebfc452f8dbcf2368a831f2fb0f240f0e135f59
Ivan Lutsenko <evanlt187@gmail.com>
2023-12-18 19:08:28 +0200

    Fixed db context

--- src/eShop.Persistence/ApplicationDbContext.cs
+++ src/eShop.Persistence/ApplicationDbContext.cs
@@ -1,12 +1,24 @@
-﻿using eShop.Persistence.Configurations;
+﻿using eShop.Domain.Customers;
+using eShop.Domain.Orders;
+using eShop.Domain.Products;
+using eShop.Persistence.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace eShop.Persistence;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions dbContextOptions)
-        : base(dbContextOptions)
+
+    public DbSet<Customer> Customers { get; init; }
+    
+    public DbSet<LineItem> LineItems { get; init; }
+    
+    public DbSet<Order> Orders { get; init; }
+    
+    public DbSet<Product> Products { get; init; }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base(options)
     {
     }
 
