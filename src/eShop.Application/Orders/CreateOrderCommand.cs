d137d066370600cf1439268debc817b552002740
Ivan Lutsenko <evanlt187@gmail.com>
2024-01-26 14:06:03 +0200

    Added something

---
+++ src/eShop.Application/Orders/CreateOrderCommand.cs
@@ -0,0 +1,21 @@
+﻿using eShop.Domain.Customers;
+using MediatR;
+using eShop.Domain.Orders;
+
+namespace eShop.Application.Orders;
+
+public sealed record CreateOrderCommand(CustomerId CustomerId) : IRequest;
+
+/*public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand>
+{
+    public CreateOrderCommandHandler(Application)
+    {
+        
+    }
+    
+    public Task Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        Order.Create()
+        throw new NotImplementedException();
+    }
+}*/
--- src/eShop.Web.API/Program.cs
+++ src/eShop.Web.API/Program.cs
@@ -1,9 +1,13 @@
+using eShop.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
